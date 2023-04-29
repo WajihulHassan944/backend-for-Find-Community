f7a171758fff4793ad2114a9c58cd72ec99df2fd
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-04-29 10:43:07 +0500

    updated functions

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/care-a
 const PORT = process.env.PORT || 3000;
 
 
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB database");
+});
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
@@ -23,90 +29,84 @@ mongoose.connect(MONGODB_URI, {
    
   });
 
-const Community = new  mongoose.model('Community', {
+const Community =  mongoose.model('Community', {
   name: String,
   interest: String,
   description: String,
 });
 
-const Event = new mongoose.model('Event', {
+const Event = mongoose.model('Event', {
   name: String,
   date: Date,
   description: String,
   location: String,
 });
 
-
 app.post('/communities', (req, res) => {
-    const community = new Community({
-      name: req.body.name,
-      interest: req.body.interest,
-      description: req.body.description,
-    });
-  
-    community.save((err) => {
-      if (err) {
-        res.status(500).send({ error: err });
-      } else {
-        res.send({ message: 'Community created successfully' });
-      }
-    });
+  const community = new Community({
+    name: req.body.name,
+    interest: req.body.interest,
+    description: req.body.description,
   });
-  
-  app.get('/communities', async (req, res) => {
-    const query = {};
-  
-    if (req.query.interest) {
-      query.interest = req.query.interest;
-    }
-  
-    try {
-      const communities = await Community.find(query);
+
+  community.save()
+    .then(() => {
+      res.send({ message: 'Community created successfully' });
+    })
+    .catch((err) => {
+      res.status(500).send({ error: err });
+    });
+});
+
+app.get('/communities', (req, res) => {
+  const query = {};
+
+  if (req.query.interest) {
+    query.interest = req.query.interest;
+  }
+
+  Community.find(query)
+    .then((communities) => {
       res.send(communities);
-    } catch (err) {
+    })
+    .catch((err) => {
       res.status(500).send({ error: err });
-    }
-  });
-  
-  app.post('/events', (req, res) => {
-    const event = new Event({
-      name: req.body.name,
-      date: req.body.date,
-      description: req.body.description,
-      location: req.body.location,
     });
-  
-    event.save((err) => {
-      if (err) {
-        res.status(500).send({ error: err });
-      } else {
-        res.send({ message: 'Event created successfully' });
-      }
-    });
+});
+
+app.post('/events', (req, res) => {
+  const event = new Event({
+    name: req.body.name,
+    date: req.body.date,
+    description: req.body.description,
+    location: req.body.location,
   });
-  
-  app.get('/events', async (req, res) => {
-    const query = {};
-  
-    if (req.query.location) {
-      query.location = req.query.location;
-    }
-  
-    try {
-      const events = await Event.find(query);
+
+  event.save()
+    .then(() => {
+      res.send({ message: 'Event created successfully' });
+    })
+    .catch((err) => {
+      res.status(500).send({ error: err });
+    });
+});
+
+app.get('/events', (req, res) => {
+  const query = {};
+
+  if (req.query.location) {
+    query.location = req.query.location;
+  }
+
+  Event.find(query)
+    .then((events) => {
       res.send(events);
-    } catch (err) {
+    })
+    .catch((err) => {
       res.status(500).send({ error: err });
-    }
-  });
+    });
+});
   
-
-
-
-
-
-
-
 
 
 app.listen(PORT, () => {
