af527be98fe2762bbc86252db7c2d9abf8edbb52
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-04-29 09:01:41 +0500

    updated with cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
